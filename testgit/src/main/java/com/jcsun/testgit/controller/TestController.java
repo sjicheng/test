977816c84bd9b1a68ac72b085109110a2e4950b1
jcsun <952099084@qq.com>
2022-03-03 16:59:05 +0800

    测试提交2

---
+++ testgit/src/main/java/com/jcsun/testgit/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.jcsun.testgit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jcsun
+ * @date 2022/3/3 16:56
+ */
+@RestController
+public class TestController {
+}
