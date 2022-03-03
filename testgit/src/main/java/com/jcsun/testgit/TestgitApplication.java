da61521a41a9a38d3f8c74820127c7fdc7e7cb6b
sjicheng <100827123+sjicheng@users.noreply.github.com>
2022-03-03 17:08:58 +0800

    Update TestgitApplication.java
    测试拉去别人的代码

--- testgit/src/main/java/com/jcsun/testgit/TestgitApplication.java
+++ testgit/src/main/java/com/jcsun/testgit/TestgitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestgitApplication {
 
     public static void main(String[] args) {
+        // 线上编辑一次  进行拉去
         SpringApplication.run(TestgitApplication.class, args);
     }
 
